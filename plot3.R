98f5808dcd65ed6e9ad6ac08cdc76090e3b53a56
Wendy Viola <wendyviola92@gmail.com>
2021-11-26 19:03:15 +0300

    peer graded assignment

---
+++ plot3.R
@@ -0,0 +1,15 @@
+power <- read.table("household_power_consumption.txt",skip=1,sep=";")
+names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+power <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
+
+
+power$POSIX <-as.POSIXlt.character(paste(power$Date,power$Time),format = "%d/%m/%Y %H:%M:%S")
+
+png("plot3.png", width=480, height=480)
+
+plot(x=power$POSIX,y=power$Sub_metering_1, type='l', col = 'black', ylab = 'Energy sub metering', xlab = '')
+lines(x=power$POSIX,y=power$Sub_metering_2, col='red')
+lines(x=power$POSIX,y=power$Sub_metering_3, col='blue')
+
+legend('topright', legend = c('Sub_metering_1',"Sub_metering_2","Sub_metering_3"), col = c('black','red','blue'), lty = 1)
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,15 @@
+power <- read.table("household_power_consumption.txt",skip=1,sep=";")
+names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+power <- power[which(power$Date == '2/2/2007' | power$Date=='1/2/2007'),]
+power$POSIX <-as.POSIXlt.character(paste(power$Date,power$Time),format = "%d/%m/%Y %H:%M:%S")
+ 
+png("plot4.png", width=480, height=480)
+par(mfrow=c(2,2))
+plot(x=power$POSIX ,y=power$Global_active_power, type = 'l', xlab='',ylab = 'Global Active Power')
+plot(x=power$POSIX ,y=power$Voltage, type = 'l', xlab='datetime',ylab = 'Voltage')
+plot(x=power$POSIX,y=power$Sub_metering_1, type='l', col = 'black', ylab = 'Energy sub metering', xlab = '')
+lines(x=power$POSIX,y=power$Sub_metering_2, col='red')
+lines(x=power$POSIX,y=power$Sub_metering_3, col='blue')
+legend('topright', legend = c('Sub_metering_1',"Sub_metering_2","Sub_metering_3"), col = c('black','red','blue'), lty = 1, bty = "n")
+plot(x=power$POSIX ,y=power$Global_reactive_power, type = 'l', xlab='datetime',ylab = 'Global_reactive_power')
+dev.off()
