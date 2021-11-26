635ee805140762f192c30f7d60a2dcc84f6b9f2d
Wendy Viola <wendyviola92@gmail.com>
2021-11-26 19:28:58 +0300

    peer graded assignment

--- plot1.R
+++ plot1.R
@@ -8,7 +8,7 @@ names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","V
 spower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
 png("plot1.png", width=480, height=480)
 
-hist(as.numeric(as.character(subpower$Global_active_power)),col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
+hist(as.numeric(as.character(spower$Global_active_power)),col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
 
 title(main="Global Active Power")
 dev.off()
